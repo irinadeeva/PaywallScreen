9a9abba02f1b743024c2063bb4199f5232ec73cc
irinadeeva <deeva97@me.com>
2025-08-05 14:21:48 +0300

    Initial Commit

---
+++ PaywallScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaywallScreen
+//
+//  Created by Irina Deeva on 05/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
